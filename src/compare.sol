45dccef1b1b5721326e8cefb4b9380bb791b773d
stefanionescu <stefanionescu@protonmail.com>
2020-08-11 01:56:40 +0100

    First commit

---
+++ src/compare.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.6.7;
+
+contract DSCompare {
+    // Uint
+    function lt(uint x, uint y) public pure returns (bool) {
+      return x < y;
+    }
+    function le(uint x, uint y) public pure returns (bool) {
+      return x <= y;
+    }
+    function gt(uint x, uint y) public pure returns (bool) {
+      return x > y;
+    }
+    function ge(uint x, uint y) public pure returns (bool) {
+      return x >= y;
+    }
+}
